dfa0c269512d24fe83b02a1e16da77d7f9b1c7c0
parker-research <166864283+parker-research@users.noreply.github.com>
2024-11-07 13:37:14 -0500

    Update docs in payload

--- isa_sim/images/vuln_demo_1/payload/injection_demo_payload_1.s
+++ isa_sim/images/vuln_demo_1/payload/injection_demo_payload_1.s
@@ -1,16 +1,23 @@
 # injection_demo_payload_1.s
 
+# // Equivalent C Code:
+# while (1) {
+#     // Display 'X' character repeatedly in simulation.
+#     sim_putc('X');
+# }
+
     .section .text
     .global _start
 
 _start:
-    li t0, 0x01000000               # Load CSR_SIM_CTRL_PUTC constant (1 << 24) into t0
-    li t1, 88                       # Load ASCII value of 'X' into t1
-    andi t1, t1, 0xFF               # Mask to ensure only the lower 8 bits are used
-    or t0, t0, t1                   # Combine CSR_SIM_CTRL_PUTC with 'X' in t0
+    li t0, 0x01000000   # Load CSR_SIM_CTRL_PUTC constant (1 << 24) into t0
+    li t1, 88           # Load ASCII value of 'X' into t1
+    andi t1, t1, 0xFF   # Mask to ensure only the lower 8 bits are used
+    or t0, t0, t1       # Combine CSR_SIM_CTRL_PUTC with 'X' in t0
 loop:
-    csrw dscratch, t0               # Write the argument to the dscratch CSR
-    j loop                          # Repeat indefinitely
+    
+    csrw dscratch, t0   # Write the argument to reg to display during simulation
+    j loop              # Repeat indefinitely
 
 # TODO: Future example: jump back in the code to where it was before.
 
